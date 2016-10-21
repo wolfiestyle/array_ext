b83f81da3a87a8c1982b224a07b8f1e1db448247
darkstalker <slayerbeast@gmail.com>
2016-10-20 21:22:48 -0300

    added more tests

--- src/tests.rs
+++ src/tests.rs
@@ -14,6 +14,15 @@ fn test_empty<T: Array<i32>>(mut arr: T)
     assert_eq!(arr.get_mut(1), None);
 }
 
+#[test]
+fn methods_empty()
+{
+    let arr: [i32; 0] = [];
+    test_empty(arr);
+    assert_eq!(arr.map(|a| a * 2), []);
+    assert_eq!(arr.fold(0, |a, n| a + n), 0);
+}
+
 fn test_arr<T: Array<V>, V: PartialEq + Debug>(mut arr: T, len: usize, mut first: V, mut last: V, mut second: V)
 {
     assert_eq!(arr.len(), len);
@@ -31,21 +40,27 @@ fn test_arr<T: Array<V>, V: PartialEq + Debug>(mut arr: T, len: usize, mut first
 #[test]
 fn methods()
 {
-    let arr: [i32; 0] = [];
-    test_empty(arr);
-    assert_eq!(arr.map(|a| a * 2), []);
-    assert_eq!(arr.fold(0, |a, n| a + n), 0);
-
     let arr = [1, 2, 3, 4];
     test_arr(arr, 4, 1, 4, 2);
     assert_eq!(arr.map(|a| a * 2), [2, 4, 6, 8]);
     assert_eq!(arr.fold(0, |a, n| a + n), 10);
+
+    let arr = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
+    test_arr(arr, 7, 'c', 'b', 'd');
+    assert_eq!(arr.map(|a| a.to_uppercase().next().unwrap()), ['C', 'D', 'E', 'F', 'G', 'A', 'B']);
+    assert_eq!(arr.fold(String::new(), |mut a, c| { a.push(c); a }), "cdefgab");
 }
 
 fn sum<T, V>(arr: T, val: V) -> T
     where T: Array<V>, V: Add<Output=V> + Copy
 {
     arr.map(|n| n + val)
+}
+
+fn avg<T: Array<f32>>(arr: T) -> f32
+{
+    let n = arr.len() as f32;
+    arr.fold(0.0, |a, n| a + n) / n
 }
 
 #[test]
@@ -56,6 +71,9 @@ fn generics()
     assert_eq!(sum([1, 2], 20), [21, 22]);
     assert_eq!(sum([1, 2, 3], 30), [31, 32, 33]);
     assert_eq!(sum([1, 2, 3, 4], 40), [41, 42, 43, 44]);
+
+    assert_eq!(avg([3.5, 4.1, 7.0, 1.5]), 4.025);
+    assert_eq!(avg([1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0, 256.0, 512.0]), 102.3);
 }
 
 #[test]
@@ -67,4 +85,8 @@ fn slice()
     let mut arr = [1, -5, 8, 42, 33];
     arr.as_mut_slice().sort();
     assert_eq!(arr, [-5, 1, 8, 33, 42]);
+
+    let mut v = Vec::new();
+    v.extend_from_slice(arr.as_slice());
+    assert_eq!(arr, &v[..]);
 }
