ea5cbafcd86abe072a425bab23156e79fba7db15
wolfiestyle <slayerbeast@gmail.com>
2022-11-10 14:29:17 -0300

    moved ArrayN into the main file

--- src/lib.rs
+++ src/lib.rs
@@ -203,8 +203,24 @@ impl<T, const N: usize> Array<T> for [T; N] {
     }
 }
 
-pub mod sized;
-pub use sized::ArrayN;
+/// Array with size information on the type.
+pub trait ArrayN<T, const N: usize>: Array<T> {
+    /// Merges elements with another array by calling a `FnMut(T, U) -> V` closure for each pair.
+    fn zip_with<U, V, F>(self, other: [U; N], f: F) -> [V; N]
+    where
+        F: FnMut(T, U) -> V;
+}
+
+impl<T, const N: usize> ArrayN<T, N> for [T; N] {
+    #[inline]
+    fn zip_with<U, V, F>(self, other: [U; N], mut f: F) -> [V; N]
+    where
+        F: FnMut(T, U) -> V,
+    {
+        let mut b = other.into_iter();
+        self.map(|a| f(a, b.next().unwrap()))
+    }
+}
 
 #[cfg(test)]
 mod tests;
